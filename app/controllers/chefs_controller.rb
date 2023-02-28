6baefa3df6c6550defb5d3e7eeb269963094ac48
Ryan Saladino <saladinoryan5@gmail.com>
2023-02-28 14:45:02 +0100

    Show Chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,19 +1,29 @@
 class ChefsController < ApplicationController
-  def new
-    @chef = Chef.new
+  before_action :find_chef, only: [:show]
+
+  def show
   end
 
-  def create
-    @chef = Chef.new(chef_params)
-    # if  @chef.save
-    # #   redirect_to (@)
-    # # else
-    # #   render :new, status: :unprocessable_entity
-    # # end
-  end
+  # def new
+  #   @chef = Chef.new
+  # end
+
+  # def create
+  #   @chef = Chef.new(chef_params)
+  #   # if  @chef.save
+  #   # #   redirect_to (@)
+  #   # # else
+  #   # #   render :new, status: :unprocessable_entity
+  #   # # end
+  # end
 
   private
-  def chef_params
-    params.require(:chef.permit(:speciality, :cat_name, :price))
+
+  def find_chef
+    @chef = Chef.find(params[:id])
   end
+
+  # def chef_params
+  #   params.require(:chef.permit(:speciality, :cat_name, :price))
+  # end
 end
