10ca758ba0e51b934e055130a24b35cd977822db
Ryan Saladino <saladinoryan5@gmail.com>
2023-02-28 15:11:06 +0100

    DEstroy chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,12 +1,12 @@
 class ChefsController < ApplicationController
-  before_action :find_chef, only: [:show]
+  before_action :find_chef, only: %i[show destroy]
 
   def show
   end
 
-  # def new
-  #   @chef = Chef.new
-  # end
+  def new
+    @chef = Chef.new
+  end
 
   # def create
   #   @chef = Chef.new(chef_params)
@@ -17,13 +17,18 @@ class ChefsController < ApplicationController
   #   # # end
   # end
 
+  def destroy
+    @chef.destroy
+    redirect_to chefs_path, status: :see_other
+  end
+
   private
 
   def find_chef
     @chef = Chef.find(params[:id])
   end
 
-  # def chef_params
-  #   params.require(:chef.permit(:speciality, :cat_name, :price))
-  # end
+  def chef_params
+    params.require(:chef.permit(:speciality, :cat_name, :price))
+  end
 end
