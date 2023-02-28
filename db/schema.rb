d31fa53664a22560c298c9ee89199229db95c581
Ryan Saladino <saladinoryan5@gmail.com>
2023-02-28 12:08:44 +0100

    Add Price + Has many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_173313) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_110731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_173313) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_chefs_on_user_id"
   end
 
