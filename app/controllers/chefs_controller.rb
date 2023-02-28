ad9589e3cd14d7edf91f4ff524ec98164693ad8b
elena chibrit <elenachibrit93@gmail.com>
2023-02-28 12:41:13 +0100

    Create, new and strong params for Chefs

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,2 +1,18 @@
 class ChefsController < ApplicationController
+  def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    @chef.save
+  end
+
+
+
+
+  private
+  def chef_params
+    params.require(:chef.permit(:speciality, :cat_name, :price))
+  end
 end
