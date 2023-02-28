7e25e9d63349539b6a8fddb1e49c72e7edfb61d5
elena chibrit <elenachibrit93@gmail.com>
2023-02-28 17:06:19 +0100

    Pulling private find

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,7 @@
 class Chef < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :cat_name, presence: true, uniqueness: true
+  validates :speciality, presence: true
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_many :chefs
   has_many :bookings
+  validates :email, presence: true
+  validates :email, format: { with: /\A.*@.*\.com\z/ }
+  validates :encrypted_password, presence: true
+  validates :encrypted_password, length: { minimum: 6 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
